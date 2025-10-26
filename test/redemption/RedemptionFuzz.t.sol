c49e801bbf1c12ccb0b782bf2d71f8d7f4907468
0xCooki <william.ridge.95@gmail.com>
2025-10-26 19:28:07 +0000

    feat: fuzz tests

--- test/redemption/RedemptionFuzz.t.sol
+++ test/redemption/RedemptionFuzz.t.sol
@@ -1,13 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.6;
 
-import {Test} from "forge-std/Test.sol";
+import {StdStorage, Test, stdStorage} from "forge-std/Test.sol";
 import {Hashes} from "contracts/Hashes.sol";
-import {Redemption, IRedemption} from "contracts/redemption/Redemption.sol";
+import {Redemption} from "contracts/redemption/Redemption.sol";
 import {DeployRedemption} from "script/DeployRedemption.s.sol";
 
 /// @dev Fuzz test suite for the Hashes DAO redemption contract
 contract RedemptionFuzzTest is Test, DeployRedemption {
+    using stdStorage for StdStorage;
+
     Hashes public hashes;
 
     modifier prank(address _caller) {
@@ -23,7 +25,59 @@ contract RedemptionFuzzTest is Test, DeployRedemption {
         run();
     }
 
-    /// fuzz over the balance for initial split
-    /// fuzz the total number redeemed for second split with no additional deposits
-    /// fuzz the total number redeemed for second split with additional deposits after first split
+    function testFuzzInitialRedemptionPerHash(uint256 _balance) public prank(redemptionMultisig) {
+        _balance = bound(_balance, 0, 1_000 ether);
+        deal(address(redemption), _balance);
+        redemption.setRedemptionStage();
+        assertLe(redemption.INITIAL_ELIGIBLE_HASHES_TOTAL() * redemption.redemptionPerHash(), _balance);
+    }
+
+    function testFuzzFinalRedemptionPerHash(uint256 _balance, uint256 _redeemed) public prank(redemptionMultisig) {
+        uint256 total = redemption.INITIAL_ELIGIBLE_HASHES_TOTAL();
+        _balance = bound(_balance, 0, total * 1e18);
+        _redeemed = bound(_redeemed, 1, total);
+
+        // Redemption Stage
+        deal(address(redemption), _balance);
+        redemption.setRedemptionStage();
+
+        uint256 initialRedemptionHash = redemption.redemptionPerHash();
+        uint256 initialETHWithdrawn = _balance - (_redeemed * initialRedemptionHash);
+        assertLe(total * initialRedemptionHash, _balance);
+
+        stdstore.target(address(redemption)).sig("totalNumberRedeemed()").checked_write(_redeemed);
+        deal(address(redemption), initialETHWithdrawn);
+
+        // PostRedemption Stage
+        vm.warp(block.timestamp + redemption.MIN_REDEMPTION_TIME() + 1);
+        redemption.setPostRedemptionStage();
+        assertLe(_redeemed * redemption.redemptionPerHash(), initialETHWithdrawn);
+    }
+
+    function testFuzzFinalRedemptionPerHashWithAddition(uint256 _balance, uint256 _redeemed, uint256 _addition)
+        public
+        prank(redemptionMultisig)
+    {
+        uint256 total = redemption.INITIAL_ELIGIBLE_HASHES_TOTAL();
+        _balance = bound(_balance, 0, total * 1e18);
+        _redeemed = bound(_redeemed, 1, total);
+        _addition = bound(_addition, 0, total * 1e18);
+
+        // Redemption Stage
+        deal(address(redemption), _balance);
+        redemption.setRedemptionStage();
+
+        // PostRedemption Stage
+        uint256 initialRedemptionHash = redemption.redemptionPerHash();
+        uint256 initialETHWithdrawn = _balance - (_redeemed * initialRedemptionHash);
+        assertLe(total * initialRedemptionHash, _balance);
+
+        stdstore.target(address(redemption)).sig("totalNumberRedeemed()").checked_write(_redeemed);
+        deal(address(redemption), initialETHWithdrawn + _addition);
+
+        // PostRedemption Stage
+        vm.warp(block.timestamp + redemption.MIN_REDEMPTION_TIME() + 1);
+        redemption.setPostRedemptionStage();
+        assertLe(_redeemed * redemption.redemptionPerHash(), initialETHWithdrawn + _addition);
+    }
 }
